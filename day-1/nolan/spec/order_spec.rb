8c00fcf9e2190c2b9e58decd35ac76b0e514813a
nolan <vo.tran.hoang.nhan@moneyforward.vn>
2023-06-11 17:43:48 +0700

    fix comment

--- day-1/nolan/spec/order_spec.rb
+++ day-1/nolan/spec/order_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe Order do
   let(:item_1) { OrderItem.new(product: "product_1", price: 25.5, discount: 5.5) }
   let(:item_2) { OrderItem.new(product: "product_2", price: 36.6, discount: 6.6) }
   let(:item_3) { OrderItem.new(product: "product_3", price: 0, discount: 0) }
+  let(:item_4) { OrderItem.new(product: "product_4", price: nil, discount: nil) }
+  let(:membership_discount) { 1.242 } # 2% of (25.5 + 36.6 + 0 + 0)
 
   describe "#add_item" do
     it "should add item to list correctly" do
@@ -18,11 +20,16 @@ RSpec.describe Order do
       expect(order.items.length).to eq 2
       order.add_item(item_3)
       expect(order.items.length).to eq 3
+      order.add_item(item_4)
+      expect(order.items.length).to eq 4
+     
+      # check order of item after add
+      expect(order.items).to match_array [item_1, item_2, item_3, item_4]
     end
   end
 
   describe "#calc_price!" do
-    let(:items) { [item_1, item_2, item_3] }
+    let(:items) { [item_1, item_2, item_3, item_4] }
 
     context "list items is empty" do
       before do
@@ -59,7 +66,6 @@ RSpec.describe Order do
 
         it "should perform calculation price & apply membership discount" do
           expect(@order.price).to eq items.map {|item| item.price}.sum
-          membership_discount = @order.price * (Order::MEMBERSHIP_DISCOUNT_PERCENT / 100.0)
           expect(@order.discount).to eq (items.map {|item| item.discount}.sum + membership_discount)
           expect(@order.final_price).to eq (items.map {|item| item.final_price}.sum - membership_discount)
         end
